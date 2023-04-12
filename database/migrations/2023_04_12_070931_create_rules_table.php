d8327e808d55429b79b2104192ed8beedc4327ce
Tri Aldy Kurniawan <122498697+Kenji2005@users.noreply.github.com>
2023-04-12 14:25:02 +0700

    Migration

---
+++ database/migrations/2023_04_12_070931_create_rules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('communication_type_id');
+            $table->foreign('communication_type_id')
+            ->references('id')
+            ->on('communication_types')
+            ->restrictOnDelete();
+            $table->string('how');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rules');
+    }
+};
---
+++ database/migrations/2023_04_12_071446_create_to_roles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('to_roles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('rule_id');
+            $table->foreign('rule_id')
+            ->references('id')
+            ->on('rules')
+            ->cascadeOnDelete();
+            $table->foreignId('role_id');
+            $table->foreign('role_id')
+            ->references('id')
+            ->on('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('to_roles');
+    }
+};
