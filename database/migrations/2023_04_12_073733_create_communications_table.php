89ab7df0dee2db32b1ebb8146333ff218979394c
Tri Aldy Kurniawan <122498697+Kenji2005@users.noreply.github.com>
2023-04-12 16:37:00 +0700

    MIGRATION

---
+++ database/migrations/2023_04_12_073733_create_communications_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('communications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('communication_type_id');
+            $table->foreign('communication_type_id')
+            ->references('id')
+            ->on('communication_types');
+            $table->date('end_at');
+            $table->longText('link');
+            $table->longText('file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('communications');
+    }
+};
