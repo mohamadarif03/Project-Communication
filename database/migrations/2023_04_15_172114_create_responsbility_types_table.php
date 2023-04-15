534d6cfc6c93470a919ded7a2aff8af3eca43b45
mohamadarif03 <pecintarealmadrid2@gmail.com>
2023-04-16 00:54:17 +0700

    .

---
+++ database/migrations/2023_04_15_172114_create_responsbility_types_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responsbility_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('color');
+            $table->longText('description')->nullable();
+            $table->string('from')->nullable();
+            $table->string('to')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responsbility_types');
+    }
+};
