b85dda426d3b95e8ca32ec7900abb3717f0f8061
Tri Aldy Kurniawan <122498697+Kenji2005@users.noreply.github.com>
2023-04-18 05:47:51 +0700

    Update responsbility.js

--- public/js/pages/responsbility.js
+++ public/js/pages/responsbility.js
@@ -185,7 +185,6 @@ function GetDataSent(page){
         success:function(response){
             $('#Sent').html('')
             if(response.data.data.length > 0){
-                console.log(response)
                 $.each(response.data.data,function(index,data){
                     var row = '<div class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] mr-3">'+
                                 '<div class="h-8 p-2 items-center w-full flex justify-between ">'+
