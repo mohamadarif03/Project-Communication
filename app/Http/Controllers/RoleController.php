613782edd062e0cd0b599ac273ba298eac1b7821
mohamadarif03 <pecintarealmadrid2@gmail.com>
2023-05-01 11:27:24 +0700

    Bug Role

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -20,7 +20,7 @@ class RoleController extends Controller
     }
     
     public function paginate(){
-        $data = Role::where('name','!=','Admin')->paginate(5);
+        $data = Role::where('name','!=','Admin')->orderBy('created_at', 'desc')->paginate(5);
 
         $links = $data->links('layouts.paginate');
         return response()->json([
