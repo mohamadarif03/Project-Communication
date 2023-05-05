35335915a0d349534d337adda1ad02e64142db48
Tri Aldy Kurniawan <122498697+Kenji2005@users.noreply.github.com>
2023-05-05 14:26:54 +0700

    tes

--- public/js/pages/projectteam.js
+++ public/js/pages/projectteam.js
@@ -1,3 +1,4 @@
+GetData()
 function GetData(){
     var spreadSheetId = $('#spreadSheetId').val()
     $.ajax({
@@ -7,10 +8,84 @@ function GetData(){
             id:spreadSheetId
         },
         success:function(response){
-            console.log(response)
+            $('#team').html('')
+            $.each(response.team,function(index,data){
+                $.each(data[1],function(index,user){
+                    if(user.profile == 'default.jpg'){
+                        var src = 'src="../default.jpg"'
+                    }else{                 
+                        var src = 'src="../storage/'+user.profile+'"'
+                    }
+                    var row = '<div class="col-span-1 w-full h-36 px-2 py-1 mt-1 rounded-md bg-white">'+
+                                '<div class="h-16 flex p-2 pt-0 w-full" style="margin-top: 30px">'+
+                                    '<img '+src+' class="h-12 w-12 rounded-circle" alt="" srcset="">'+
+                                    '<div class="ml-2 flex my-auto">'+
+                                        '<div class="my-auto">'+
+                                            '<h3 class="text-xs font-bold text-slate-900 my-0">'+user.name+'</h3>'+
+                                            '<h3 class="text-xs text-slate-500 my-0">'+user.email+'</h3>'+
+                                            '<h3 class="text-xs text-black rounded-lg font-bold my-0 absolute" style="background-color: #D9D9D9; color:black; padding:2px">'+data[0]+'</h3>'+
+                                        '</div>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>'
+                    $('#team').append(row)
+                })
+            })
+
+
+            $('#creative').html('')
+            $.each(response.creative,function(index,data){
+                $.each(data[1],function(index,user){
+                    if(user.profile == 'default.jpg'){
+                        var src = 'src="../default.jpg"'
+                    }else{                 
+                        var src = 'src="../storage/'+user.profile+'"'
+                    }
+                    var row = '<div class="col-span-1 w-full h-36 px-2 py-1 mt-1 rounded-md bg-white">'+
+                                '<div class="h-16 flex p-2 pt-0 w-full" style="margin-top: 30px">'+
+                                    '<img '+src+' class="h-12 w-12 rounded-circle" alt="" srcset="">'+
+                                    '<div class="ml-2 flex my-auto">'+
+                                        '<div class="my-auto">'+
+                                            '<h3 class="text-xs font-bold text-slate-900 my-0">'+user.name+'</h3>'+
+                                            '<h3 class="text-xs text-slate-500 my-0">'+user.email+'</h3>'+
+                                            '<h3 class="text-xs text-black rounded-lg font-bold my-0 absolute" style="background-color: #D9D9D9; color:black; padding:2px">'+data[0]+'</h3>'+
+                                        '</div>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>'
+                    $('#creative').append(row)
+                })
+            })
+
+            $('#chaperone').html('')
+            $.each(response.chaperone,function(index,data){
+                $.each(data[1],function(index,user){
+                    if(user.profile == 'default.jpg'){
+                        var src = 'src="../default.jpg"'
+                    }else{                 
+                        var src = 'src="../storage/'+user.profile+'"'
+                    }
+                    var row = '<div class="col-span-1 w-full h-36 px-2 py-1 mt-1 rounded-md bg-white">'+
+                                '<div class="h-16 flex p-2 pt-0 w-full" style="margin-top: 30px">'+
+                                    '<img '+src+' class="h-12 w-12 rounded-circle" alt="" srcset="">'+
+                                    '<div class="ml-2 flex my-auto">'+
+                                        '<div class="my-auto">'+
+                                            '<h3 class="text-xs font-bold text-slate-900 my-0">'+user.name+'</h3>'+
+                                            '<h3 class="text-xs text-slate-500 my-0">'+user.email+'</h3>'+
+                                            '<h3 class="text-xs text-black rounded-lg font-bold my-0 absolute" style="background-color: #D9D9D9; color:black; padding:2px">'+data[0]+'</h3>'+
+                                        '</div>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>'
+                    $('#chaperone').append(row)
+                })
+            })
         },
         error:function(response){
-
+            console.log(response)
         }
     })
 }
+
+
+                
--- resources/views/user/projectTeam.blade.php
+++ resources/views/user/projectTeam.blade.php
@@ -44,78 +44,33 @@
             </a>
         </div>
         <div class="w-full flex flex-col">
-            <div class="grid mx-3 gap-3 grid-cols-1 md:grid-cols-4 lg:grid-cols-4 mt-4" id="Data">
-                <div class="col-span-1 w-full h-36 px-2 py-1 mt-1 rounded-md bg-white">
-                    <div class="h-16 flex p-2 pt-0 w-full" style="margin-top: 30px">
-                        <div class="h-12 flex w-12 rounded-circle" style="background-color: red">
-                            <p class="text-white m-auto font-semibold"> MA </p>
-                        </div>
-                        <div class="ml-2 flex my-auto">
-                            <div class="my-auto">
-                                <h3 class="text-xs font-bold text-slate-900 my-0">Mohamad Arif</h3>
-                                <h3 class="text-xs text-slate-500 my-0">mohamadarif@gmail.com</h3>
-                                <h3 class="text-xs text-black rounded-lg font-bold text-slate-500 my-0 absolute" style="background-color: #D9D9D9; color:black; padding:2px">service manager</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                
+            <div class="grid mx-3 gap-3 grid-cols-1 md:grid-cols-4 lg:grid-cols-4 mt-4" id="team">
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
             </div>
             <div class="flex flex-col mt-2 w-full">
                 <h1 class="text-xl font-semibold ml-3 text-black">Creatives</h1>
             </div>
-            <div class="grid mx-3 gap-3 grid-cols-1 md:grid-cols-4 lg:grid-cols-4 mt-4" id="Data">
-               
-                
-                <div class="col-span-1 w-full h-36 px-2 py-1 mt-1 rounded-md bg-white">
-                    <div class="h-16 flex p-2 pt-0 w-full" style="margin-top: 30px">
-                        <div class="h-12 flex w-12 rounded-circle" style="background-color: red">
-                            <p class="text-white m-auto font-semibold"> MA </p>
-                        </div>
-                        <div class="ml-2 flex my-auto">
-                            <div class="my-auto">
-                                <h3 class="text-xs font-bold text-slate-900 my-0">Mohamad Arif</h3>
-                                <h3 class="text-xs text-slate-500 my-0">mohamadarif@gmail.com</h3>
-                                <h3 class="text-xs text-black rounded-lg font-bold text-slate-500 my-0 absolute" style="background-color: #D9D9D9; color:black; padding:2px">service manager</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                    
+            <div class="grid mx-3 gap-3 grid-cols-1 md:grid-cols-4 lg:grid-cols-4 mt-4" id="creative">
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
             </div>
             <div class="flex flex-col mt-2 w-full">
                 <h1 class="text-xl font-semibold ml-3 text-black">Chaperones</h1>
             </div>
-            <div class="grid mx-3 gap-3 grid-cols-1 md:grid-cols-4 lg:grid-cols-4 mt-4" id="Data">
-                <div class="col-span-1 w-full h-36 px-2 py-1 mt-1 rounded-md bg-white">
-                    <div class="h-16 flex p-2 pt-0 w-full" style="margin-top: 30px">
-                        <div class="h-12 flex w-12 rounded-circle" style="background-color: red">
-                            <p class="text-white m-auto font-semibold"> MA </p>
-                        </div>
-                        <div class="ml-2 flex my-auto">
-                            <div class="my-auto">
-                                <h3 class="text-xs font-bold text-slate-900 my-0">Mohamad Arif</h3>
-                                <h3 class="text-xs text-slate-500 my-0">mohamadarif@gmail.com</h3>
-                                <h3 class="text-xs text-black rounded-lg font-bold text-slate-500 my-0 absolute" style="background-color: #D9D9D9; color:black; padding:2px">service manager</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-span-1 w-full h-36 px-2 py-1 mt-1 rounded-md bg-white">
-                    <div class="h-16 flex p-2 pt-0 w-full" style="margin-top: 30px">
-                        <div class="h-12 flex w-12 rounded-circle" style="background-color: red">
-                            <p class="text-white m-auto font-semibold"> MA </p>
-                        </div>
-                        <div class="ml-2 flex my-auto">
-                            <div class="my-auto">
-                                <h3 class="text-xs font-bold text-slate-900 my-0">Mohamad Arif</h3>
-                                <h3 class="text-xs text-slate-500 my-0">mohamadarif@gmail.com</h3>
-                                <h3 class="text-xs text-black rounded-lg font-bold text-slate-500 my-0 absolute" style="background-color: #D9D9D9; color:black; padding:2px">service manager</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="grid mx-3 gap-3 grid-cols-1 md:grid-cols-4 lg:grid-cols-4 mt-4" id="chaperone">
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
+                <div class="col-span-1 h-36 bg-slate-300 rounded-md animate-pulse"></div>
             </div>         
         </div>
     </div>
@@ -124,6 +79,6 @@
 @endsection
 
 @section('javascript')
-    <script src="{{ asset('js/pages/project.js') }}"></script>
+    <script src="{{ asset('js/pages/projectteam.js') }}"></script>
 @endsection
 
