99fdca6adfbc6a9bf57d3259d2244c11fd5273de
Tri Aldy Kurniawan <122498697+Kenji2005@users.noreply.github.com>
2023-05-16 08:58:34 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function view(Request $request)
+    public function view()
     {
         return view('admin.users');
     }
